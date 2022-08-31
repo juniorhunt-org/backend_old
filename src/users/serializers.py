42b5e352d08c971ee165217746ba7efd7642b842
Ivaneev Ivan <ivaneev.ivane@gmail.com>
2022-08-31 14:05:51 +0300

    add profile id

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -6,7 +6,8 @@ from .models import ProfileUser
 class ProfileUserSerializer(ModelSerializer):
     class Meta:
         model = ProfileUser
-        fields = (
-            'first_name', 'last_name', 'second_name', 'address', 'description', 'avatar', 'company_name', 'gender',
-            'user_id',
-            'contacts', 'is_company')
+        fields = ('id',
+                  'first_name', 'last_name', 'second_name', 'address', 'description', 'avatar', 'company_name',
+                  'gender',
+                  'user_id',
+                  'contacts', 'is_company')
