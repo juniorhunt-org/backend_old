a5729346b783f3233aaf759fbe160c13999c1b96
Ivaneev Ivan <ivaneev.ivane@gmail.com>
2022-11-24 17:52:17 +0300

    Fixed a bug related to the container not seeing Django

--- src/start.bash
+++ src/start.bash
@@ -1,5 +1,5 @@
 cd /code/
-
+poetry install
 poetry run python manage.py collectstatic --no-input
 poetry run python manage.py migrate
 poetry run python -m gunicorn --bind 0.0.0.0:8000 core.wsgi --workers=4
